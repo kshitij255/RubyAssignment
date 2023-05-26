8af6bfbc60feacf80d8c50bd6f7f89149be62f9e
kshitij255 <134687456+kshitij255@users.noreply.github.com>
2023-05-26 14:03:32 +0530

    Update Problem2.rb

--- Problem2.rb
+++ Problem2.rb
@@ -1,13 +1,18 @@
+=begin
+    Given the following data structures, write a program that copies the information 
+    from the array into the empty hash that applies to the correct person.
+=end
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
-["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
-contact_data.each do |data|
- contacts.each do |key,value|
- name = key.split(' ')
-  if data.at(0).include?(name.first.downcase)
-    contacts[key] = data
-   end
- end
-end
-puts contacts
+
+i=0
+contacts.each do |key,value|                                   # Traversing and adding values in hash
+    # puts "#{key},#{value}"
+    contacts[key][:email] = contact_data[i][0]
+    contacts[key][:address] = contact_data[i][1]
+    contacts[key][:phone] = contact_data[i][2]
+    i+=1
+end 
+puts contacts                             # Printing output
