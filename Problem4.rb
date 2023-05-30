e52fbf3ef6431b861d0d397762340625116a5959
kshitij255 <134687456+kshitij255@users.noreply.github.com>
2023-05-30 14:11:01 +0530

    Problem4.rb

--- Problem4.rb
+++ Problem4.rb
@@ -1,10 +1,20 @@
-array = ["Red", "Green", "Blue", "White"]        
-value = gets.chomp                               
-ans = false
-array.each do |element|                             
-    if(element==value)
+class OperationOnArray
+  
+  def checkForColor(array,value)
+    ans = false
+    array.each do |element|                             
+      if(element==value)
         ans=true
-    end    
-end
-puts ans                                        
-     
+      end    
+    end
+    puts ans 
+  end  
+  
+end  
+
+obj = OperationOnArray.new
+array = ["Red", "Green", "Blue", "White"]  
+
+puts "Enter the value"
+value = gets.chomp
+obj.checkForColor(array,value)
