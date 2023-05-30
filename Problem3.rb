ff857caf19a3c5246d28582deceb252ec3e3297d
kshitij255 <134687456+kshitij255@users.noreply.github.com>
2023-05-30 13:21:15 +0530

    Problem3.rb

--- Problem3.rb
+++ Problem3.rb
@@ -1,14 +1,19 @@
-def remove_first_last(str,strLength)     
-    return str.slice(2..strLength)
+class OperationOnString
+  
+  def removeFirstLastCharacters(string)
+    strLength = string.length
+    if(strLength<=4)
+      puts "String length is less than 4."
+    else    
+      puts "After removing the first and last two characters from string."     
+      result = string.slice(2..strLength-3)            
+      puts result                                             
+    end 
+  end  
+  
 end
 
-puts "Enter the string : "           
-str = gets.chomp                           
-strLength = str.length
-if(strLength<=4)
-    puts "String length is less than 4."
-else    
-    puts "After removing the first and last two characters from string."     
-    result = remove_first_last(str,strLength-3)            
-    puts result                                             
-end  
+puts "Enter the string"
+str = gets.chomp                     
+obj = OperationOnString.new
+obj.removeFirstLastCharacters(str)
