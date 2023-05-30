567a14f14e5bdef8c212c27fae92b6a58740a0ba
kshitij255 <134687456+kshitij255@users.noreply.github.com>
2023-05-30 12:42:09 +0530

    Problem3.rb

--- Problem3.rb
+++ Problem3.rb
@@ -1,22 +1,16 @@
-=begin
-    Write a Ruby program to remove the first and last two characters from a specified string.
-    Sample Output:
-    JavaScript -> vaScri
-    Python -> th
 
-=end
 
-def remove_first_last(str,strLength)       #Function to remove first and last two characters from string
+def remove_first_last(str,strLength)     
     return str.slice(2..strLength)
 end
 
 puts "Enter string : "           
-str = gets.chomp                           # Getting input from user
+str = gets.chomp                           
 strLength = str.length
-if(strLength<=4)
+if(strLength<=4
     puts "String length is less than 4."
 else    
     puts "After removing the first and last two characters from string."     
-    result = remove_first_last(str,strLength-3)             # Calling function
-    puts result                                             # Printing result
+    result = remove_first_last(str,strLength-3)            
+    puts result                                             
 end  
