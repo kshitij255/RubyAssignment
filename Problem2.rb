b28a432f3eae09c3482f6fff0900ed11627119a5
kshitij255 <134687456+kshitij255@users.noreply.github.com>
2023-05-30 14:54:46 +0530

    Problem2.rb

--- Problem2.rb
+++ Problem2.rb
@@ -1,14 +1,23 @@
+class Test
+  
+  def assignData(contact_data,contacts)
+    
+    i=0
+    contacts.each do |key,value|
+      contacts[key][:email] = contact_data[i][0]
+      contacts[key][:address] = contact_data[i][1]
+      contacts[key][:phone] = contact_data[i][2]
+      i+=1
+     end 
+    puts contacts  
+  end  
+  
+end  
+
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
             ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-i=0
-contacts.each do |key,value|                                   # Traversing and adding values in hash
-    # puts "#{key},#{value}"
-    contacts[key][:email] = contact_data[i][0]
-    contacts[key][:address] = contact_data[i][1]
-    contacts[key][:phone] = contact_data[i][2]
-    i+=1
-end 
-puts contacts                             # Printing output
+obj = Test.new
+obj.assignData(contact_data,contacts)
