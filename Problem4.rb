8839a0cef4e8fe08840d523de9f8368c54a2926e
kshitij255 <134687456+kshitij255@users.noreply.github.com>
2023-05-30 12:42:58 +0530

    Problem4.rb

--- Problem4.rb
+++ Problem4.rb
@@ -1,24 +1,10 @@
-=begin
-    Write a Ruby program to check whether a value exists in an array.
-    Sample Output:
-    Original array:
-    ["Red", "Green", "Blue", "White"]
-    Check if 'Green' in color array!
-    true
-    Check if 'Pink' in color array!
-    false
-=end
-
-array = ["Red", "Green", "Blue", "White"]        # Given Array
-value = gets.chomp                               # Getting input from user
+array = ["Red", "Green", "Blue", "White"]        
+value = gets.chomp                               
 ans = false
-array.each do |ele|                              # Traversing each element in array
+array.each do |ele|                             
     if(ele==value)
         ans=true
     end    
 end
-puts ans                                         # Printing answer    
-
-# Using inbuilt function
-# puts array.include?("Green")
-# puts array.include?("Pink")        
+puts ans                                        
+     
