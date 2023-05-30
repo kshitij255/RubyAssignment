4a9aac1303685cbaa871b523596e80abe910472e
kshitij255 <134687456+kshitij255@users.noreply.github.com>
2023-05-30 13:09:12 +0530

    Problem1.rb

--- Problem1.rb
+++ Problem1.rb
@@ -1,15 +1,30 @@
-hash = { a:1, b:2, c:3, d:4 }               
-valueOfKeyB = hash[:b]
-puts "Value of key ':b' : #{valueOfKeyB}";     
-
-puts "Hash after adding key:value pair `{e:5}"
-hash[:e]=5;                        
-hash.each do |key,value|                      
-    puts "key : #{key}  Value : #{value}"
+class OperationsOnHash
+  
+  def valueOfKeyB(hash)
+    value = hash[:b]
+    puts "Value of key ':b' : #{value}";  
+  end  
+  
+  def addingKeyValuePair(hash)
+    puts "Hash after adding key:value pair `{e:5}"
+    hash[:e]=5;                        
+    hash.each do |key,value|                      
+      puts "key : #{key}  Value : #{value}"
+    end
+  end
+  
+  def removingKeyValuePair(hash)
+     puts "Hash after Removing all key:value pairs whose value is less than 3.5"
+    hash.delete_if{|key,value| value<3.5}
+    hash.each do |key,value|                      
+      puts "key : #{key}  Value : #{value}"
+    end
+  end  
+  
 end
 
-puts "Hash after Removing all key:value pairs whose value is less than 3.5"
-hash.delete_if{|key,value| value<3.5}
-hash.each do |key,value|                      
-    puts "key : #{key}  Value : #{value}"
-end
+hash = { a:1, b:2, c:3, d:4 }
+obj = OperationsOnHash.new
+obj.valueOfKeyB(hash)
+obj.addingKeyValuePair(hash)
+obj.removingKeyValuePair(hash)
